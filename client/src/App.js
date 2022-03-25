4eea83b94bdb4df9cbb1f4390bba22d158c819ad
kayrux <skusjanto@outlook.com>
2022-03-25 00:14:04 -0600

    Added Login.js and basic functionality to log in

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,16 @@ import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Navbar from './components/Navbar';
 import Footer from "./components/Footer";
+import Login from "./pages/login/Login";
 
 const App = () => {
     const [showLoggedAsAdmin, setLoggedAsAdmin] = useState(false) // Keep track of whether the user is logged in as admin
 
+    // TO DO: interacts with database to verify before logging in
+    const login = (loginInfo) => {
+        console.log("Logging in") // Assume login successful
+        setLoggedAsAdmin(true) 
+    }
 
     return ( 
         <Router>
@@ -17,7 +23,7 @@ const App = () => {
                     <Route path="/professors" element={<h1>Professors</h1>}></Route>
                     <Route path="/faculties" element={<h1>Faculties</h1>}></Route>
                     <Route path="/reports" element={<h1>Reports</h1>}></Route>
-                    <Route path="/login" element={<h1>Login</h1>}></Route>
+                    <Route path="/login" element={<Login onLogin={login}/>}></Route>
                 </Routes>
                 <Footer />
             </div>
