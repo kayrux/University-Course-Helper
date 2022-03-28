b310fbccc2951ae687208948c59efeabc1a4beb9
Anika Achari <anika3040@gmail.com>
2022-03-28 17:32:57 -0600

    changed name

--- client/src/pages/professorList/ProfessorList.js
+++ client/src/pages/professorList/ProfessorList.js
@@ -3,7 +3,7 @@ import ListDisplay from '../../components/ListDiplay';
 
 const ProfessorList = () => {
 
-    const [professorList, setProfessorList] = useState(["Eberly", "Pavol", "Reda", "Kashfia"])
+    const [professorList, setProfessorList] = useState(["Wayne Eberly", "Pavol Federl", "Reda Elhajj", "Kashfia Sailunaz"])
 
     const type = "professor"
 
