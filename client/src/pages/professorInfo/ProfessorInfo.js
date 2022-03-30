e486694f4eafa0ffad6b462cbcb885c9a8586cc2
Anika Achari <anika3040@gmail.com>
2022-03-29 20:02:02 -0600

    prof info

---
+++ client/src/pages/professorInfo/ProfessorInfo.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const ProfessorInfo = () => {
+    return (
+        <div className="display-container">
+            <h1>Professor Information</h1>
+            <h3>Professor Description</h3>
+            <p1>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                Nulla consequat massa quis enim.
+            </p1>
+            <h3>Courses Taught</h3>
+            <p1>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                Nulla consequat massa quis enim.
+            </p1>
+            <h3>Professor Pages</h3>
+            <p1>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                Nulla consequat massa quis enim.
+            </p1>
+        </div>
+    )
+}
+
+export default ProfessorInfo
