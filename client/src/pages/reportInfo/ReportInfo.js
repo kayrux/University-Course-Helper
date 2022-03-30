4a859518eec4b4177442755697b47e41fef3177c
Anika Achari <anika3040@gmail.com>
2022-03-29 22:50:06 -0600

    created report info

---
+++ client/src/pages/reportInfo/ReportInfo.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const ReportInfo = () => {
+    return (
+        <div className="display-container">
+            <h1>Report Information</h1>
+            <h3>User Account Number:</h3>
+    
+            <h3>Report Type:</h3>
+            <p1>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                Nulla consequat massa quis enim.
+            </p1>
+            <h3>Reason for Report:</h3>
+            <p1>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                Nulla consequat massa quis enim.
+            </p1>
+            <h3>Rating Information:</h3>
+            <p1>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                Nulla consequat massa quis enim.
+            </p1>
+            <h3>Course Name and Number:</h3>
+            <p1>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                Nulla consequat massa quis enim.
+            </p1>
+            <h3>Score:</h3>
+            <p1>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                Nulla consequat massa quis enim.
+            </p1>
+            <h3>Comment:</h3>
+            <p1>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                Nulla consequat massa quis enim.
+            </p1>
+       
+       
+       
+        </div>
+    )
+}
+
+export default ReportInfo
