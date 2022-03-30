74d5c54a290d1ab21b45b11a434e2808d431575c
kayrux <skusjanto@outlook.com>
2022-03-29 19:06:37 -0600

    Added working FacultyInfo.js

---
+++ client/src/pages/facultyInfo/FacultyInfo.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const FacultyInfo = () => {
+    return (
+        <div className="display-container">
+            <h1>Faculty Information</h1>
+            <h3>Major Degrees</h3>
+            <p1>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                Nulla consequat massa quis enim.
+            </p1>
+            <h3>Minor Degrees</h3>
+            <p1>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                Nulla consequat massa quis enim.
+            </p1>
+            <h3>Courses</h3>
+            <p1>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                Nulla consequat massa quis enim.
+            </p1>
+        </div>
+    )
+}
+
+export default FacultyInfo
