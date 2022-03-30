8031273a46572815f8c3e0f534081d54e9a0ad0a
kayrux <skusjanto@outlook.com>
2022-03-29 18:25:50 -0600

    Moved Course display code into CouseInfo.js

--- client/src/pages/courseInfo/CourseInfo.js
+++ client/src/pages/courseInfo/CourseInfo.js
@@ -1,16 +1,43 @@
 import { useState } from "react"
 import ListDisplay from '../../components/ListDiplay';
+import AddCommentContainer from "../courseInfo/AddCommentContainer"
 
 const CourseInfo = () => {
-
-    const [courseInformation, setCourseInformation] = useState(["Courses", "Professor Links", "Department Links", "Degree Links", "Comments/Ratings"])
-
-    const type = "course"
-
     return(
-        <div>
-            <ListDisplay list = {courseInformation} type = {type} />
-        </div>
+        <div className="courseInfo-display">
+        <h1>Course Information</h1>
+        <h3>Course description</h3>
+        <p1>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo 
+            ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, 
+            nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
+            Nulla consequat massa quis enim.
+        </p1>
+        <h3>Professors</h3>
+        <p1>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo 
+            ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, 
+            nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
+            Nulla consequat massa quis enim.
+        </p1>
+        <h3>Department</h3>
+        <p1>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo 
+            ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, 
+            nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
+            Nulla consequat massa quis enim.
+        </p1>
+        <h3>Degree</h3>
+        <p1>
+            Lorem ipsum dolor sit amet
+        </p1>
+        
+        <h3>Comments</h3>
+        <p1>
+            Lorem ipsum dolor sit amet
+        </p1>
+        <AddCommentContainer />
+    </div>
     )
 }
 
