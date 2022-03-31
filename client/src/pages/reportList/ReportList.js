5ddb3922a8d9d0753e88aad1bc71df3d809af6ab
Anika Achari <anika3040@gmail.com>
2022-03-31 10:51:13 -0600

    capitalized report numbers

--- client/src/pages/reportList/ReportList.js
+++ client/src/pages/reportList/ReportList.js
@@ -4,7 +4,7 @@ import ListDisplay from '../../components/ListDiplay';
 const ReportList = () => {
 
     //This is the list of reports to be displayed
-    const [reportList, setReportList] = useState(["report 1", "report 2", "report 3", "report 4", "report 5"])
+    const [reportList, setReportList] = useState(["Report 1", "Report 2", "Report 3", "Report 4", "Report 5"])
 
     //This is used to tell ListDisplay which page to render the list (one of /,/professors,/faculties,/reports)
     const type = "report"
