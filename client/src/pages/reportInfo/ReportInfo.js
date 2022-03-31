40311bbff9c68d72d57bcf84ec8473424dcf725d
DrCoconuttt <Richardharder100@gmail.com>
2022-03-30 18:29:02 -0600

    Updated Report info page

--- client/src/pages/reportInfo/ReportInfo.js
+++ client/src/pages/reportInfo/ReportInfo.js
@@ -9,25 +9,23 @@ const ReportInfo = () => {
     return (
         <div className="display-container">
             <h1>Report Information</h1>
-            <h3>User Account Number</h3>
-            <p>23483718</p>
-            <h3>Report Type:</h3>
+            <h3>Report Type</h3>
             <p1>Racism</p1>
             <h3>Reason for Report</h3>
             <p1>The comment is racist</p1>
             <hr />
-            <h1>Rating Information</h1>
+            <h1>Rating Reported</h1>
             <h3>Course Name and Number</h3>
             <p1>
-                CPSC 313
+                CPSC 331
             </p1>
             <h3>Score</h3>
             <p1>
                 2.5/5
             </p1>
-            <h3>Comment:</h3>
+            <h3>Comment</h3>
             <p1>
-                Insert comment here
+                This course reminds me of *insert something racist
             </p1>
             <h3>Date</h3>
             <p1>30/03/2022</p1>
